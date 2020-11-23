2dda38bc9ff4592279cf3e1a171556198904cf54
andrewwmclain <mclaina1@spu.edu>
2020-11-22 18:51:33 -0800

    message

---
+++ public/javascripts/books.js
@@ -0,0 +1,31 @@
+document.getElementById("submitNewBook").addEventListener("click",
+    function(){
+// //         console.log("book added!");
+// //
+//         var url = 'https://csc3221-final-project-back-end.herokuapp.com/api/books';
+//
+//         let name = document.getElementById("namePost").value;
+//         let author = document.getElementById("authorPost").value;
+//         let isbn = document.getElementById("isbnPost").value;
+//         let price = document.getElementById("pricePost").value;
+//
+//         var params = 'name='+name+'author='+author+'isbn='+isbn+'price='+price;
+//
+//         var xhttp = new XMLHttpRequest();
+//
+//         xhttp.onreadystatechange = function() {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 window.location.href = '/add.html';
+//             }
+//         };
+//
+//         xhttp.open("POST", url,
+//             true);
+//         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//         xhttp.send(params);
+    }
+    )
+
+// var redirect = function(){
+//     document.location.href="add.html"
+// }
